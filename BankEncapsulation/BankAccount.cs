0e758ec7196b711d246fe7c4263fa75f25e0ff24
Todd Barrett <toddcbarrett3@gmail.com>
2023-06-30 11:50:26 -0700

    Completed Bank Account Exercise with added features

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0; 
+
+        public void Deposit(double deposit)
+        {
+            _balance += deposit;
+            Console.WriteLine($"Your current balance is ${_balance}");
+        }
+        public void GetBalance() 
+        {
+            Console.WriteLine($"Your current balance is ${_balance}");            
+        }
+        public void Withdraw(double withdraw)
+        {
+            _balance -= withdraw;
+            Console.WriteLine($"Your current balance is ${_balance}");
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace BankEncapsulation
+﻿using System.ComponentModel.Design;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+            var exit = true;
+            do
+            {
+                Console.WriteLine("Welcome:");
+                Console.WriteLine("To check balance: Press 1");
+                Console.WriteLine("To deposit money: Press 2");
+                Console.WriteLine("To withdraw money: Press 3");
+                Console.WriteLine("To exit the system: Press 4");
+                var userSelection = int.Parse(Console.ReadLine());
+ 
+                if (userSelection == 4)
+                {
+                    Console.WriteLine("Thank you for your business!");
+                    exit = false;
+                    break;
+                }
+                else
+                {
+                    switch (userSelection)
+                    {
+                        case 1:
+                            account.GetBalance();
+                            break;
+                        case 2:
+                            Console.WriteLine($"How much do you want to deposit?");
+                            var userInputDeposit = double.Parse(Console.ReadLine());
+                            account.Deposit(userInputDeposit);
+                            break;
+                        case 3:
+                            Console.WriteLine($"How much do you want to withdraw?");
+                            var userInputWithdraw = double.Parse(Console.ReadLine());
+                            account.Withdraw(userInputWithdraw);
+                            break;
+                        default:
+                            Console.WriteLine("please enter a valid selection");
+                            break;
+                    }
+                }
+            } while (exit == true);
         }
     }
 }
